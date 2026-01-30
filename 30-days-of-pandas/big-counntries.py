6a34664df7c805f596362b70b81aa46f23ab57a3
Mansi Wakharkar <mansiswakharkar@gmail.com>
2026-01-30 18:13:31 +0530

    Leetcode - Day 1 - 30 days of pandas

--- 30-days-of-pandas/big-counntries.py
+++ 30-days-of-pandas/big-counntries.py
@@ -3,4 +3,4 @@
 import pandas as pd
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    return world[(world['population']>25000000) & (world['area']>3000000)][['name', 'population', 'area']]
+    return world[(world['population']>=25000000) | (world['area']>3000000)][['name', 'population', 'area']]
