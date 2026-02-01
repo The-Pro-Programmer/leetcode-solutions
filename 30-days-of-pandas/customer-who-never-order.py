95b81ff87aa678822c65390d5f13772085011b33
Mansi Wakharkar <mansiswakharkar@gmail.com>
2026-02-01 17:10:21 +0530

    Leetcode - Day 3 - 30 days of pandas

---
+++ 30-days-of-pandas/customer-who-never-order.py
@@ -0,0 +1,6 @@
+## https://leetcode.com/problems/customers-who-never-order/?envType=study-plan-v2&envId=30-days-of-pandas&lang=pythondata
+
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    return customers[~customers['id'].isin(orders['customerId'])][['name']].rename(columns={'name': 'Customers'})
